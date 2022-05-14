8d5bef875d6ed3bed71175b53b668b06824a0e85
Matheus Ricardo Tomas <matheusrcdtms@hotmail.com>
2022-05-14 19:11:53 -0300

    Created Planets

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing_extensions import Self
 import pygame
 import math
 pygame.init()
@@ -7,6 +8,32 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
+
+class Planet:
+    
+    AU = 149.6e6 * 1000
+    G = 6.67428e-11
+    SCALE = 250 / AU # 1AU = 1000 pixels
+    TIMESTAMP = 3600*24 # 1 day
+    
+    def __init__(self, x, y, radius, color, mass):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.mass = mass
+        
+        self.orbit = []
+        self.sun = False
+        self.distance_to_sun = 0
+        
+        self.x_vel = 0
+        self.y_vel = 0
+    
+    def draw(self,win):
+        x = self.x * self.SCALE + WIDTH / 2
+        y = self.y * self.SCALE + HEIGHT / 2
+        pygame.draw.circle(win, self.color, (x,y), self.radius)
 
 def main():
     run = True
