178d69b8e5578af03eb4a536d9d19a865c786f87
Matheus Ricardo Tomas <matheusrcdtms@hotmail.com>
2022-05-14 20:06:07 -0300

    Drawing orbits

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from turtle import update
 import pygame
 import math
 pygame.init()
@@ -7,6 +8,7 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 YELLOW = (255, 255, 0)
 BLUE = (100, 149, 237)
 RED = (188, 39, 50)
@@ -36,8 +38,50 @@ class Planet:
     def draw(self,win):
         x = self.x * self.SCALE + WIDTH / 2
         y = self.y * self.SCALE + HEIGHT / 2
-        pygame.draw.circle(win, self.color, (x,y), self.radius)
+        
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                x,y = point
+                x = x * self.SCALE + WIDTH / 2
+                y = y * self.SCALE + WIDTH / 2
+                updated_points.append((x,y))
+            
+            pygame.draw.lines(win, self.color, False, updated_points, 2)            
+            pygame.draw.circle(win, self.color, (x,y), self.radius)
+    
+    def attraction(self, other):
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = (distance_x**2 + distance_y**2)**0.5
+        
+        if other.sun:
+            self.distance_to_sun = distance
+        
+        force = self.G*self.mass*other.mass/distance**2
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta)*force
+        force_y = math.sin(theta)*force
+        
+        return force_x, force_y
 
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+            
+        self.x_vel += total_fx / self.mass * self.TIMESTAMP
+        self.y_vel += total_fy / self.mass * self.TIMESTAMP
+        
+        self.x += self.x_vel * self.TIMESTAMP
+        self.y += self.y_vel * self.TIMESTAMP
+        self.orbit.append((self.x, self.y))
+    
 def main():
     run = True
     clock = pygame.time.Clock()
@@ -45,21 +89,27 @@ def main():
     sun = Planet(0, 0, 30, YELLOW, 1.98892e30)
     sun.sun = True
     
-    mercury = Planet(-0.387*Planet.AU, 0, 8, DARK_GREY, 3.30e23)
-    venus = Planet(-0.723*Planet.AU, 0, 14, WHITE, 4.8685e24)
+    mercury = Planet(0.387*Planet.AU, 0, 8, DARK_GREY, 3.30e23)
+    mercury.y_vel = -47.4e3
+    venus = Planet(0.723*Planet.AU, 0, 14, WHITE, 4.8685e24)
+    venus.y_vel = -35.02e3
     earth = Planet(-1*Planet.AU, 0, 16, BLUE, 5.9742e24)
+    earth.y_vel = 29.783e3
     mars = Planet(-1.524*Planet.AU, 0 , 12, RED, 6.39e23) 
+    mars.y_vel = 24.077e3
     
     planets = [sun, earth, mars, mercury, venus]
     
     while run:
         clock.tick(60)
+        WIN.fill(BLACK)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WIN)
         
         pygame.display.update()
